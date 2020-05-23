afcbcc5701757b4049ccd35f05969c021871a392
Dinh Quoc Thuy <qthuy.0081@gmail.com>
2020-05-23 21:13:32 +0700

    Init project

---
+++ Todolist/Todolist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todolist
+//
+//  Created by Dinh Quoc Thuy on 5/23/20.
+//  Copyright © 2020 Dinh Quoc Thuy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
